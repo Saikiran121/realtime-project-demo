a9a6df632acf0a00e6bae539106b3bc5f60e9790
Saikiran Biradar <saikiranbiradar76642@gmail.com>
2025-07-12 23:25:40 +0530

    Updated pom.xml as per java 17 and CustomerController as per Sonar

--- src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
+++ src/main/java/com/cloudtechmasters/realtimeprojectdemo/CustomerController.java
@@ -1,5 +1,7 @@
 package com.cloudtechmasters.realtimeprojectdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,10 @@ import java.util.Arrays;
 import java.util.List;
 @RestController
 public class CustomerController {
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
     @GetMapping("/getAllCustomers")
     public List<Customer> getAllCustomers(){
+	logger.info("Get All customers");
         Customer customer1=new Customer(100,"vamsikrm");
         Customer customer2=new Customer(200,"harsha");
         return Arrays.asList(customer1,customer2);
